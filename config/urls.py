0b6ba7444da5daed300a5482cec303bad7f0c277
sajjad <faniam321@gmail.com>
2022-05-08 14:12:24 +0430

    add new app to settings and urls

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', include('users.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
