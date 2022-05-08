177461a464d80bda2d668712f04641d92972f783
sajjad <faniam321@gmail.com>
2022-05-09 00:00:41 +0430

    add new model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'language', 'created_at', 'updated_at')
+    list_filter = ('language',)
+    ordering = ('-created_at',)
+
+
+admin.site.register(Account, AccountAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Language(models.TextChoices):
+    PERSIAN = ('Fa', 'fa')
+    ENGLISH = ('En', 'en')
+
+
+class Account(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    photo = models.ImageField(
+        upload_to='profile-photo/', null=True, blank=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
+    language = models.CharField(
+        max_length=10, choices=Language.choices, default=Language.PERSIAN)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
