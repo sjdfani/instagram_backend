0437b5bdd58ecccedbae3c9ff0238483bc8522da
sajjad <faniam321@gmail.com>
2022-05-08 16:25:46 +0430

    complete custom users

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
     list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
