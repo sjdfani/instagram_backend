4102dd5016210b06f276603562c8d718f3f56de5
sajjad <faniam321@gmail.com>
2022-05-08 23:33:17 +0430

    add new app to urls and settings

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users.apps.UsersConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', include('users.urls')),
+    path('account/', include('account.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
