cf31f27d04fa7b6a8ace9a53471a865f74025bf2
sajjad <faniam321@gmail.com>
2022-05-08 16:16:18 +0430

    add new model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
