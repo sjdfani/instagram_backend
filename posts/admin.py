263d876c9f08cd1b260ccdc9ad1769cbacbca6a6
sajjad <faniam321@gmail.com>
2022-05-12 18:42:45 +0430

    add new model

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Tags
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('account', 'created_at', 'updated_at', 'status')
     list_filter = ('account', 'status')
     ordering = ('-created_at',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('post',  'name', 'status', 'created_at')
+    list_filter = ('post', 'status')
+    ordering = ('-created_at',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tags, TagAdmin)
