f0c614413b6572e5ecd980afabc2b7bdee1348ac
sajjad <faniam321@gmail.com>
2022-05-25 11:20:19 +0430

    add new model

--- follows/admin.py
+++ follows/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Following
 
-# Register your models here.
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('account', 'following', 'created_at')
+    list_filter = ('account',)
+
+
+admin.site.register(Following, FollowingAdmin)
--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from account.models import Account
 
-# Create your models here.
+
+class Following(models.Model):
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='following_acc')
+    following = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='following_foll')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.account.user.email
