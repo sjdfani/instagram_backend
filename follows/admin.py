015663b7d4501c0a193f6d4109e06b6066edc0b5
sajjad <faniam321@gmail.com>
2022-05-28 18:30:56 +0430

    create new model

--- follows/admin.py
+++ follows/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Following
+from .models import Follower, Following
 
 
 class FollowingAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class FollowingAdmin(admin.ModelAdmin):
     list_filter = ('account',)
 
 
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ('account', 'follower', 'created_at')
+    list_filter = ('account',)
+
+
 admin.site.register(Following, FollowingAdmin)
+admin.site.register(Follower, FollowerAdmin)
--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from account.models import Account
 
@@ -7,6 +8,17 @@ class Following(models.Model):
         Account, on_delete=models.CASCADE, related_name='following_acc')
     following = models.ForeignKey(
         Account, on_delete=models.CASCADE, related_name='following_foll')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.account.user.email
+
+
+class Follower(models.Model):
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='follower_acc')
+    follower = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='follower_foll')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
