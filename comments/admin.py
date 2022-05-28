300fbbde920562e82d1a0a4617fd0cef60cba652
sajjad <faniam321@gmail.com>
2022-05-28 19:54:27 +0430

    add new model

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Comments
 
-# Register your models here.
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('post', 'author')
+
+
+admin.site.register(Comments, CommentsAdmin)
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from posts.models import Post
+from account.models import Account
 
-# Create your models here.
+
+class Comments(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments_post')
+    content = models.TextField()
+    author = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='comments_acc')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.post.account.user.email
