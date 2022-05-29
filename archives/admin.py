b053b2b36d41aa59dc3c12a80595e69ca54c1298
sajjad <faniam321@gmail.com>
2022-05-30 00:08:04 +0430

    add new model

--- archives/admin.py
+++ archives/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Archive
 
-# Register your models here.
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ('account', 'post', 'created_at', 'updated_at')
+    list_filter = ('account', 'post')
+    ordering = ('-created_at',)
+
+
+admin.site.register(Archive, ArchiveAdmin)
--- archives/models.py
+++ archives/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from posts.models import Post
+from account.models import Account
 
-# Create your models here.
+
+class Archive(models.Model):
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='archive_acc')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='archive_post')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.account.user.email
