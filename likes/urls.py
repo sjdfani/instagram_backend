9bdeec97ac0365dae590dd3936792bb80de0915e
sajjad <faniam321@gmail.com>
2022-05-31 17:49:39 +0430

    complete like process

--- likes/urls.py
+++ likes/urls.py
@@ -5,6 +5,6 @@ app_name = 'likes'
 
 urlpatterns = [
     path('create/', CreateLike.as_view()),
-    path('list/<int:pk>/', ListLike.as_view()),
+    path('list/post/<int:pk>/', ListLike.as_view()),
     path('list/ret-des/<int:pk>/', RetrieveDestroyLike.as_view()),
 ]
