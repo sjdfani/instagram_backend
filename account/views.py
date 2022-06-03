10c3272b7c99f2820bce15c18dd024bd821c3c1c
sajjad <faniam321@gmail.com>
2022-06-03 11:46:47 +0430

    remove extra class

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from .models import Account
-from .serializer import AccountSerializer, UpdateInformationSerializer, ChangeLanguageSerializer, ChangeProfilePhotoSerializer, SetBirthdateSerializer, CountPostFollowsSerializer
+from .serializer import AccountSerializer, UpdateInformationSerializer, ChangeLanguageSerializer, ChangeProfilePhotoSerializer, SetBirthdateSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
