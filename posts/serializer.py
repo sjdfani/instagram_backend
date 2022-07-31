97d3ba7a949360ce48087ba4c3c3193eacaf7787
sajjad <faniam321@gmail.com>
2022-07-31 09:56:16 +0430

    delete account_likes

--- posts/serializer.py
+++ posts/serializer.py
@@ -59,9 +59,6 @@ class ListPostSerializer(serializers.ModelSerializer):
         res['tags'] = TagsSerializer(instance.tags, many=True).data
         res['account'] = AccountSerializer(
             instance.account, context={'request': request}).data
-        like_objects = Like.objects.filter(
-            post=instance.id).values_list('account__id', flat=True)
-        res['account_likes'] = list(set(like_objects))
         archive_objects = Archive.objects.filter(
             post=instance.id).values_list('account__id', flat=True)
         res['account_archives'] = list(set(archive_objects))
@@ -111,9 +108,6 @@ class RetrievePostSerializer(serializers.ModelSerializer):
         res['tags'] = TagsSerializer(instance.tags, many=True).data
         res['account'] = AccountSerializer(
             instance.account, context={'request': request}).data
-        like_objects = Like.objects.filter(
-            post=instance.id).values_list('account__id', flat=True)
-        res['account_likes'] = list(set(like_objects))
         archive_objects = Archive.objects.filter(
             post=instance.id).values_list('account__id', flat=True)
         res['account_archives'] = list(set(archive_objects))
